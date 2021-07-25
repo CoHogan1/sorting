f3093f4400f008747a0090f1217cfa1f21b7ae88
Conner Hogan <ConnerRHogan@gmail.com>
2021-07-25 17:04:16 -0600

    continued education

--- sorting_algo/quicksort/quicksort.js
+++ sorting_algo/quicksort/quicksort.js
@@ -2,6 +2,11 @@ let list = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]
 // [ 1, 1, 2, 2, 4, 8, 32, 43, 43, 55, 63, 92, 123, 123, 234, 345, 5643 ]
 
 
+// Though sort() gives the result we want, problem lies with the way it sorts 
+// the array elements. Default sort() in JavaScript uses insertion sort by
+// V8 Engine of Chrome and Merge sort by Mozilla Firefox and Safari.
+
+
 const quickSort = (array) => {
     if (array.length === 1){
         return array
